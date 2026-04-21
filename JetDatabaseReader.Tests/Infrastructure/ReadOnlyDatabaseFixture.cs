752d39ab7196dee07378dab7fbc6f7585fe41bd4
Edward Miller <edward.miller@mesirow.com>
2026-04-21 18:06:38 -0500

    remove unused file

--- JetDatabaseReader.Tests/Infrastructure/ReadOnlyDatabaseFixture.cs
+++
@@ -1,9 +0,0 @@
-namespace JetDatabaseReader.Tests;
-
-using Xunit;
-
-[CollectionDefinition(DisableParallelization = false)]
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1515:Consider making public types internal", Justification = "xUnit CollectionDefinition requires public accessibility")]
-public sealed class ReadOnlyDatabaseFixture : ICollectionFixture<DatabaseCache>
-{
-}
